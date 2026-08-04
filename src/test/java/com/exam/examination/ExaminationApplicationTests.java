968b186ffc98d8d8ecf81a84ee446f6d191d9c3f
Urvashi Thakur <ut472003@gmail.com>
2026-08-04 14:24:49 +0530

    Examination Service Complete

--- src/test/java/com/exam/examination/ExaminationApplicationTests.java
+++ src/test/java/com/exam/examination/ExaminationApplicationTests.java
@@ -1,17 +1,27 @@
 package com.exam.examination;
 
+import com.exam.examination.repository.AssessmentAttemptRepository;
+import com.exam.examination.repository.AssessmentQuestionRepository;
+import com.exam.examination.repository.AssessmentRepository;
+import com.exam.examination.repository.QuestionRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-})
 class ExaminationApplicationTests {
+
+    @MockitoBean
+    private AssessmentRepository assessmentRepository;
+
+    @MockitoBean
+    private QuestionRepository questionRepository;
+
+    @MockitoBean
+    private AssessmentQuestionRepository assessmentQuestionRepository;
+
+    @MockitoBean
+    private AssessmentAttemptRepository assessmentAttemptRepository;
 
     @Test
     void contextLoads() {
